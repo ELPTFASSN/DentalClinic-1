a4edc81a54930d5a3809d981897c8a7f3fb6cd4d
priyarvyas <vyaspriya5@gmail.com>
2018-04-16 13:37:38 -0400

    added attributes in insurance and medical history classes

--- DentalClinic/InsuranceDetail.cs
+++ DentalClinic/InsuranceDetail.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace DentalClinic
 {
+    enum HealthCardCompany
+    {
+        OHIP, BLueCross, SunLife, FlexCare
+    }
     class InsuranceDetail
     {
+        string healthCardNumber;
+        string fullName;
+        DateTime expiryDate;
+        string company;
+
+        public string HealthCardNumber { get => healthCardNumber; set => healthCardNumber = value; }
+        public string FullName { get => fullName; set => fullName = value; }
+        public DateTime ExpiryDate { get => expiryDate; set => expiryDate = value; }
+        public string Company { get => company; set => company = value; }
     }
 }
--- DentalClinic/MedicalHistory.cs
+++ DentalClinic/MedicalHistory.cs
@@ -8,5 +8,12 @@ namespace DentalClinic
 {
     class MedicalHistory
     {
+        string allergies;
+        string heartDisease;
+        bool bloodPressure;
+
+        public string Allergies { get => allergies; set => allergies = value; }
+        public string HeartDisease { get => heartDisease; set => heartDisease = value; }
+        public bool BloodPressure { get => bloodPressure; set => bloodPressure = value; }
     }
 }
