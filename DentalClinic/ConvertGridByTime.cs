68e17c41f96a5f4531775336bed1dbf61b037334
priyarvyas <vyaspriya5@gmail.com>
2018-04-20 19:46:01 -0400

    added my converter class

---
+++ DentalClinic/ConvertGridByTime.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace DentalClinic
+{
+    [ValueConversion(typeof(string), typeof(Brush))]
+    class ConvertGridByTime : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string TimePast10 = value.ToString();
+            return TimePast10 == "23:10" ? Brushes.Red : Brushes.White;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Brushes.White;
+        }
+    }
+}
